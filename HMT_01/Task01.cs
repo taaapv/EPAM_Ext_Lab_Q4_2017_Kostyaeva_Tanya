ec752c94668be1c6f2e07fc1df57a9b5a82c1918
taaapv <34029493+taaapv@users.noreply.github.com>
2017-11-28 15:33:59 +0400

    Update Task01.cs

--- HMT_01/Task01.cs
+++ HMT_01/Task01.cs
@@ -1,13 +1,22 @@
-﻿using System;
-
 /*Написать консольное приложение, которое проверяет принадлежность точки заштрихованной области.
 Пользователь вводит координаты точки (x; y) и выбирает букву графика (a-к). В консоли должно высветиться сообщение: «Точка [(x; y)] принадлежит фигуре [г]».*/
-
-namespace decision
+namespace Decision
 {
-    class Program
+    using System;
+
+    public class Program
     {
-        static void Main()
+        public static void MessageEnable(double x, double y, string text)
+        {
+            Console.WriteLine('\n' + $"Точка [{x}; {y}] принадлежит фигуре [{text}]" + '\n' + '\n');
+        }
+
+        public static void MessageDisable(double x, double y, string text)
+        {
+            Console.WriteLine('\n' + $"Точка [{x}; {y}] не принадлежит фигуре [{text}]" + '\n' + '\n');
+        }
+
+        public static void Main()
         {
             try
             {
@@ -17,23 +26,11 @@ namespace decision
                 double y = double.Parse(Console.ReadLine());
                 Console.WriteLine("Введите букву графика (а - к)");
                 string contur = string.Concat(Console.ReadLine());
-                                      
-                if (contur == "а" || contur == "А") //фигура А
-                {
-                    if ((x * x) + (y * y) <= 1) 
-                    {
-                        MessageEnable(x, y, contur);                        
-                    }
-                    else
-                    {
-                        MessageDisable(x, y, contur);
-                    }
-                    throw new ApplicationException();
-                }
 
-                if (contur == "б" || contur == "Б") //фигура Б
+                ////фигура А
+                if (contur == "а" || contur == "А")
                 {
-                    if ((((Math.Abs(x * x) + Math.Abs(y * y)) <= 1) && (Math.Abs(x * x) + Math.Abs(y * y)) >= 0.25)) 
+                    if ((x * x) + (y * y) <= 1)
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -41,12 +38,14 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
-                if (contur == "в" || contur == "В") //фигура В
+                ////фигура Б
+                if (contur == "б" || contur == "Б")
                 {
-                    if (Math.Abs(x) <= 1 && Math.Abs(y) <= 1)  
+                    if (((Math.Abs(x * x) + Math.Abs(y * y)) <= 1) && (Math.Abs(x * x) + Math.Abs(y * y)) >= 0.25)
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -54,13 +53,14 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
-
-                if (contur == "г" || contur == "Г") //фигура Г
+                ////фигура В
+                if (contur == "в" || contur == "В")
                 {
-                    if ((Math.Abs(x) + Math.Abs(y)) <= 1)  
+                    if (Math.Abs(x) <= 1 && Math.Abs(y) <= 1)
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -68,13 +68,14 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
-
-                if (contur == "д" || contur == "Д") //фигура Д
+                ////фигура Г
+                if (contur == "г" || contur == "Г")
                 {
-                    if ((Math.Abs(x) + 0.5 * Math.Abs(y)) <= 0.5)  
+                    if ((Math.Abs(x) + Math.Abs(y)) <= 1)
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -82,13 +83,14 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
-
-                if (contur == "е" || contur == "Е") //фигура Е
+                ////фигура Д
+                if (contur == "д" || contur == "Д")
                 {
-                    if ((x >= 0) && (x * x + y * y <= 1) || (x < 0) && (x >= -2) && (Math.Abs(x) + 2 * Math.Abs(y) <= 2))   
+                    if (((Math.Abs(x) + 0.5) * Math.Abs(y)) <= 0.5)
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -96,13 +98,14 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
-
-                if (contur == "ж" || contur == "Ж") //фигура Ж
+                ////фигура Е
+                if (contur == "е" || contur == "Е")
                 {
-                    if (((y + 2 * x) <= 2) && ((y - 2 * x) <= 2) && (y >= -1) && (y <= 2))   
+                    if (((x >= 0) && ((x * x) + (y * y) <= 1)) || ((x < 0) && (x >= -2) && (Math.Abs(x) + (2 * Math.Abs(y)) <= 2)))
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -110,12 +113,14 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
-                if (contur == "з" || contur == "З") //фигура З
+                ////фигура Ж
+                if (contur == "ж" || contur == "Ж")
                 {
-                    if ((y <= 1) && (y >= -2) && (x <= 1) && (x >= -1) && (Math.Abs(x) + Math.Abs(y - 1)) >= 1)  
+                    if (((y + (2 * x)) <= 2) && ((y - (2 * x)) <= 2) && (y >= -1) && (y <= 2))
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -123,12 +128,14 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
-                if (contur == "и" || contur == "И") //фигура И
+                ////фигура З
+                if (contur == "з" || contur == "З")
                 {
-                    if (((y > 0) && (x + y <= 0) && (y - 2 * x <= 3)) || ((y <= 0) && ((y - 2 * x) <= 3) && ((x - 3 * y) <= 1)))
+                    if ((y <= 1) && (y >= -2) && (x <= 1) && (x >= -1) && (Math.Abs(x) + Math.Abs(y - 1)) >= 1)
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -136,12 +143,14 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
-                if (contur == "к" || contur == "к") //фигура К
+                ////фигура И
+                if (contur == "и" || contur == "И")
                 {
-                    if (((y >= 1) && (x <= 1)) || ((y >= 1) && (x >=1 )) || ((x <= y) && (x > -1) && (x < 1)) || (x + y >= 0) && ((x > -1) && (x < 1)))
+                    if (((y > 0) && (x + y <= 0) && (y - (2 * x) <= 3)) || ((y <= 0) && ((y - (2 * x)) <= 3) && ((x - (3 * y)) <= 1)))
                     {
                         MessageEnable(x, y, contur);
                     }
@@ -149,36 +158,38 @@ namespace decision
                     {
                         MessageDisable(x, y, contur);
                     }
+
                     throw new ApplicationException();
                 }
 
+                ////фигура К
+                if (contur == "к" || contur == "к")
+                {
+                    if (((y >= 1) && (x <= 1)) || ((y >= 1) && (x >= 1)) || ((x <= y) && (x > -1) && (x < 1)) || ((x + y >= 0) && ((x > -1) && (x < 1))))
+                    {
+                        MessageEnable(x, y, contur);
+                    }
+                    else
+                    {
+                        MessageDisable(x, y, contur);
+                    }
+
+                    throw new ApplicationException();
+                }
                 else
                 {
                     Console.WriteLine('\n' + $"Графика с буквой '{contur}' не существует!" + '\n');
                     Console.ReadKey();
-                }                
+                }
             }
-                        
             catch (ApplicationException)
             {
-                              
+                Console.WriteLine();
             }
             catch (SystemException ex)
             {
                 Console.WriteLine('\n' + $"Ошибка ввода данных: {ex}!" + '\n');
             }
-        }
-        
-        static void MessageEnable (double x, double y, string text)
-        {
-            Console.WriteLine('\n' + $"Точка [{x}; {y}] принадлежит фигуре [{text}]" + '\n' + '\n');
-        }
-        
-        static void MessageDisable (double x, double y, string text)
-        {
-            Console.WriteLine('\n' + $"Точка [{x}; {y}] не принадлежит фигуре [{text}]" + '\n' + '\n');
-        }
+        }        
     }
-}      
-    
-
+}
